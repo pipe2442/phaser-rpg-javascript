c220f119d1165dbef7cbe932fe19c7b5fe6171b7
pipe2442 <felipeangulo2442@gmail.com>
2021-07-08 17:50:13 -0500

    Add walking animations - and collide with objects

--- js/game.js
+++ js/game.js
@@ -69,6 +69,36 @@ var WorldScene = new Phaser.Class({
 	    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         this.cameras.main.startFollow(this.player);
         this.cameras.main.roundPixels = true;
+
+        //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        // animation with key 'right'
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14]}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 6, 0, 12 ] }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, obstacles);
     },
     
     update: function(time, delta) {
@@ -93,7 +123,31 @@ var WorldScene = new Phaser.Class({
         else if (this.cursors.down.isDown)
         {
             this.player.body.setVelocityY(80);
-        }  
+        }
+        
+
+        //Add player movement animation
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.anims.play('right', true);
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.player.anims.play('up', true);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.anims.play('down', true);
+        }
+        else
+        {
+            this.player.anims.stop();
+        }
     }
 
 });
